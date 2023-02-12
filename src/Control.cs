43fc7ee769d12a39c326b76bf98ca068411f7ce3
cuong02n <cuong02n@gmail.com>
2023-02-12 16:56:17 +0700

    ok

--- src/Control.cs
+++ src/Control.cs
@@ -1,5 +1,7 @@
-namespace CoTyPhu; 
+namespace CoTyPhu;
 
 public class Control {
-    
+    public int handle_turn(int player, int dice1, int dice2) {
+        return player + ((dice1 == dice2) ? 0 : 1);
+    }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,7 @@
 namespace CoTyPhu;
 
 static class Program {
-    /// <summary>
-    ///  The main entry point for the application.
-    /// </summary>
+    
     [STAThread]
     static void Main() {
         // To customize application configuration such as set high DPI settings or default font,
