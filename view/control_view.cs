a8430dc805f81f7af9a205dba0503f1c06f46e2b
Pham Thu Hang <hangdt31999@gmail.com>
2023-02-12 22:53:15 +0700

    A

--- view/control_view.cs
+++ view/control_view.cs
@@ -1,5 +1,36 @@
 namespace CoTyPhu.view; 
 
 public class control_view {
+    public void rollTheDice()
+    {
 
+    }
+    public void jail()
+    {
+
+    }
+    public void randomEvent() {
+
+    }
+    public void turn() { 
+    
+    }
+    public void position() { 
+    
+    }
+    public void move() { 
+    
+    }
+    public void transfer() { 
+    
+    }
+    public void auction() { 
+    
+    }
+    public void item() { 
+    
+    }
+    public void teleport() { 
+    
+    }
 }
--- view/playForm.Designer.cs
+++ view/playForm.Designer.cs
@@ -45,7 +45,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::CoTyPhu.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
             this.Controls.Add(this.button1);
             this.Name = "playForm";
             this.Text = "playForm";
