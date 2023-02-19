38e7d97b49ee0c33ac0d8667ad12fa84fa77506b
cuong02n <cuong02n@gmail.com>
2023-02-19 17:54:03 +0700

    playerss

--- main.cs
+++ main.cs
@@ -4,36 +4,31 @@ using CoTyPhu.control.client;
 using CoTyPhu.model;
 
 namespace CoTyPhu;
+
 using CoTyPhu.view;
 
 class main {
-<<<<<<< HEAD
-    public static TextBox ERR =null;
-    
-=======
+    public static TextBox ERR = null;
     public static userForm user;
     public static playForm _playForm;
     public static waitingForm waitingForm;
     public static string name;
     public static int room;
->>>>>>> 8a2c1094c2061fcd29b553e68346e4b0eafcebad
     public static int room_receive_from_server;
     public static lobby l;
     public static game g;
     public static bool is_server = false;
 
-    public static int my_STT=0;
+    public static int my_STT = 0;
 
     public static control_server ControlServer;
     public static control_client ControlClient;
 
     public static Thread thresh_receive;
+
     [STAThread]
     static void Main() {
-
         ApplicationConfiguration.Initialize();
         Application.Run(new mainForm());
-        
-        
     }
 }
