037e837b07348314d05275de8f190e443ae9eeac
cuong02n <cuong02n@gmail.com>
2023-02-20 16:51:24 +0700

    20/2/2023_3

--- view/mainForm.cs
+++ view/mainForm.cs
@@ -17,6 +17,7 @@ namespace CoTyPhu.view
         public mainForm()
         {
             InitializeComponent();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
